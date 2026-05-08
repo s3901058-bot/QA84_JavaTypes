5426fe5be84d8be410b7416a6a4a42a9b87c9c21
Nikolay <s3901058@gmail.com>
2026-05-08 10:35:54 +0200

    add 1

--- src/main/java/MainJavaTypes.java
+++ src/main/java/MainJavaTypes.java
@@ -1,5 +1,43 @@
 public class MainJavaTypes {
     public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        byte a = 10;   //маленькое целое число
+        short b = 1000; // целое число
+        int age = 25; // обычное целое число
+        long population = 8000000000L; // большое целое число
+
+        float price = 10.5f; // дробное число
+        double salary = 2500.75;// дробное число, точнее чем float
+
+        char grade = 'A'; // один символ
+        boolean isActive = true; // правда/ложь
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(age);
+        System.out.println(population);
+        System.out.println(price);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isActive);
+
+
+        String str = "Hello";
+        String str1 = str;
+        String str2 = str1;
+        System.out.println(str2);
+
+    }
+}
+
+
+
+
+
+
+/*public class MainJavaTypes {
+    public static void main(String[] args) {
 
         int age = 25;
         double price = 19.99;
@@ -37,3 +75,5 @@ public class MainJavaTypes {
         }
     }
 }
+
+ */
